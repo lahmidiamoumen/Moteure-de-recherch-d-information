861fa08a213a1394027434f645384e862257c04e
Lahmidi Abdelmoumene <45982077+lahmidiamoumen@users.noreply.github.com>
2019-02-21 23:56:10 +0100

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,24 @@
+package project.model;
+
+public class Pair<L,R> {
+    private L l;
+    private R r;
+    public Pair(L l, R r){
+        this.l = l;
+        this.r = r;
+    }
+    public Pair(){};
+    public L getKey(){ return l; }
+    public R getValue(){ return r; }
+    public void setKey(L l){ this.l = l; }
+    public void setValue(R r){ this.r = r; }
+    public void put(L l,R r){ this.l = l;
+        this.r = r; }
+
+    @Override
+    public String toString() {
+        return "[" + l +
+                ", " + r +
+                ']';
+    }
+}
